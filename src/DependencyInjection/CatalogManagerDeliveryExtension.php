9ef4e6c6ef9cbe399586552c9f7d61fa5e8868a5
Alexander Naumov <me@alexandernaumov.de>
2018-09-04 14:50:07 +0200

    add services

--- src/DependencyInjection/CatalogManagerDeliveryExtension.php
+++ src/DependencyInjection/CatalogManagerDeliveryExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 
-class DeliveryBundleExtension extends Extension {
+class CatalogManagerDeliveryExtension extends Extension {
 
 
     public function load( array $configs, ContainerBuilder $container ) {
