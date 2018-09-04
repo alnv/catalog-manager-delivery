4daa7d0042be2ee01c989fb9b4c8fe1f3afddae1
Alexander Naumov <me@alexandernaumov.de>
2018-09-04 16:02:00 +0200

    add delivery api routings

--- src/Controller/DeliveryController.php
+++ src/Controller/DeliveryController.php
@@ -2,8 +2,44 @@
 
 namespace CatalogManager\DeliveryBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use CatalogManager\DeliveryBundle\Helpers\Help as Help;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
-class DeliveryController {
 
-    //
+/**
+ *
+ * @Route("/delivery-api", defaults={"_scope" = "frontend", "_token_check" = false})
+ */
+class DeliveryController extends Controller {
+
+
+    /**
+     *
+     * @Route("/{alias}", name="delivery")
+     * @Method({"GET"})
+     */
+    public function delivery( $alias ) {
+
+        $this->container->get( 'contao.framework' )->initialize();
+
+        $arrReturn = [];
+        $objDatabase = \Database::getInstance();
+        $objDelivery = $objDatabase->prepare( 'SELECT * FROM tl_deliveries WHERE alias = ? OR id = ?' )->execute( $alias, (int) $alias );
+
+        if ( !$objDelivery->numRows ) {
+
+            throw new \CoreBundle\Exception\PageNotFoundException( 'Page not found: ' . \Environment::get('uri') );
+        }
+
+        $arrDelivery = Help::parseDelivery( $objDelivery->row() );
+
+        //
+
+        header('Content-Type: application/json');
+        echo json_encode( $arrDelivery );
+        exit;
+    }
 }
