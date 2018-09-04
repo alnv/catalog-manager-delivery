8a6545611b3d96e41a02141b1c661d7f8716a42f
Alexander Naumov <me@alexandernaumov.de>
2018-09-04 14:51:37 +0200

    add be module delivery

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,13 @@
 <?php
 
-//
+array_insert( $GLOBALS['BE_MOD']['catalog-manager-extensions'], 1, [
+
+    'delivery' => [
+
+        'name' => 'delivery',
+        'tables' => [
+
+            'tl_deliveries'
+        ]
+    ]
+]);
