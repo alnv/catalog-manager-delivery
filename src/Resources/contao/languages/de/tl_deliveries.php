ab5484738e2b52ada4dd9b4c781c6cc889c1f343
Alexander Naumov <me@alexandernaumov.de>
2018-09-04 14:50:38 +0200

    add labels

---
+++ src/Resources/contao/languages/de/tl_deliveries.php
@@ -0,0 +1,26 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_deliveries']['return_settings']  = 'Rückgabe';
+$GLOBALS['TL_LANG']['tl_deliveries']['data_settings']  = 'Datensätze';
+$GLOBALS['TL_LANG']['tl_deliveries']['general_settings']  = 'Allgemein';
+$GLOBALS['TL_LANG']['tl_deliveries']['expert_settings']  = 'Erweiterte-Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_deliveries']['alias'] = [ 'Alias', 'Bitte geben Sie einen Alias ein.' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['table'] = [ 'Tabelle', 'Bitte wählen Sie eine Tabelle aus.' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['template'] = [ 'Template', 'Bitte wählen Sie ein Template aus.' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['match'] = [ 'Filter', 'Hier können Sie Ihre Auswahl begrenzen.' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['name'] = [ 'Bezeichnung', 'Bitte geben Sie eine Bezeichnung ein.' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['order'] = [ 'Sortieren', 'Hier können Sie Ihre Auswahl sortieren.' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['return'] = [ 'Rückgabe', 'Bitte wählen Sie einen Rückgabe-Typ aus.' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['globals'] = [ 'Globale Variablen', 'Hier können Sie globale Variablen für die Stylesheets des Theme definieren (z.B. $rot -> c00 oder $abstand -> 12px).' ];
+
+$GLOBALS['TL_LANG']['tl_deliveries']['new'] = [ 'Erstellen', '' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['edit'] = [ 'Bearbeiten', '' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['show'] = [ 'Anzeigen', '' ];
+$GLOBALS['TL_LANG']['tl_deliveries']['delete'] = [ 'Löschen', '' ];
+
+$GLOBALS['TL_LANG']['tl_deliveries']['reference']['return'] = [
+
+    'json' => 'JSON',
+    'template' => 'Template'
+];
